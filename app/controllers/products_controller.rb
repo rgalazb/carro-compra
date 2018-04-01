c884f4d1f76760dd6c050267463e4b6f3997bf34
rgalazb <galaz.becerra@gmail.com>
2018-04-01 17:06:57 -0300

    busquedas

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,15 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
-    if current_user.present?
-      @cantidad = current_user.cart.inject(0) { |sum, order| sum += order.quantity }
+    if params[:buscar].present?
+      @products = Product.where('name like ?', "%#{params[:buscar]}%")
+      respond_to do |format|
+        format.js
+      end
+    else
+      @products = Product.all
+      if current_user.present?
+        @cantidad = current_user.cart.inject(0) { |sum, order| sum += order.quantity }
+      end
     end
   end
 end
